59700187dbb13e1916c855add68cf43096e641a8
Steven Van Impe <steven.vanimpe@icloud.com>
2021-12-07 13:23:59 +0100

    Execute commands with the user's $SHELL. Fixes #2

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,8 +4,11 @@ import * as path from 'path';
 
 /**
  * Asynchronous wrapper around {@link cp.exec child_process.exec}.
+ * 
+ * Commands will be executed by the user's `$SHELL`, if configured.
  */
 export async function exec(command: string, options: cp.ExecOptions): Promise<{ stdout: string; stderr: string }> {
+    options.shell = process.env.SHELL;
     return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => 
         cp.exec(command, options, (error, stdout, stderr) => {
             if (error) {
