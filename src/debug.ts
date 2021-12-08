f113db52d6c09204cbf2c83e482c93936887d5bf
Adam Fowler <adamfowler71@gmail.com>
2021-12-08 18:37:10 +0000

    Add license header

--- src/debug.ts
+++ src/debug.ts
@@ -1,6 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VSCode Swift open source project
+//
+// Copyright (c) 2021 the VSCode Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VSCode Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import * as vscode from 'vscode';
 import { SwiftContext } from './SwiftContext';
-import { exec } from './utilities';
 
 // Edit launch.json based on contents of Swift Package
 // Adds launch configurations based on the executables in Package.swift
