cc434b3a71d3a854d405b1d2ffd0fa54496787fd
Tim <0xtimc@gmail.com>
2021-12-08 18:22:31 +0000

    Add missing headers

--- src/sourcekit-lsp/inlayHints.ts
+++ src/sourcekit-lsp/inlayHints.ts
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VSCode Swift open source project
+//
+// Copyright (c) 2021 the VSCode Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VSCode Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 'use strict';
 import * as vscode from 'vscode';
 import * as langclient from 'vscode-languageclient/node';
