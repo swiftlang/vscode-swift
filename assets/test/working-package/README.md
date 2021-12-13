c4dde02ed53040bde892093a19b1dd9e76fb2067
Adam Fowler <adamfowler71@gmail.com>
2021-12-13 18:00:50 +0000

    Basic package tests with assets

---
+++ assets/test/working-package/README.md
@@ -0,0 +1,3 @@
+# working-package
+
+A description of this package.
---
+++ src/test/suite/SwiftPackage.text.ts
@@ -0,0 +1,33 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VSCode Swift open source project
+//
+// Copyright (c) 2021 the VSCode Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VSCode Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import * as assert from 'assert';
+import { SwiftPackage } from '../../SwiftPackage';
+
+suite('SwiftPackage Test Suite', () => {
+	test('No package', async () => {
+        let spmPackage = await SwiftPackage.create('assets/test/empty-folder');
+        assert.strictEqual(spmPackage.targets.length, 0);
+    });
+
+	test('Invalid package', async () => {
+        let spmPackage = await SwiftPackage.create('assets/test/invalid-package');
+        assert.strictEqual(spmPackage.targets.length, 0);
+    });
+
+	test('Working package', async () => {
+        let spmPackage = await SwiftPackage.create('assets/test/working-package');
+        assert.strictEqual(spmPackage.targets.length, 2);
+    });
+});
