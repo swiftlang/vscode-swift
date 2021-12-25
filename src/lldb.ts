5b1443e62f3533312bed13b7ff4d1ea88363b8c0
Adam Fowler <adamfowler71@gmail.com>
2021-12-25 12:00:41 +0000

    Fix LLDB config setup

--- src/lldb.ts
+++ src/lldb.ts
@@ -15,6 +15,8 @@
 // Based on code taken from CodeLLDB https://github.com/vadimcn/vscode-lldb/
 // LICENSED with MIT License
 
+import * as path from "path";
+import * as fs from "fs/promises";
 import { execFile } from "./utilities";
 
 /**
@@ -29,10 +31,54 @@ export async function getLLDBLibPath(executable: string): Promise<string | undef
         const { stdout } = await execFile(executable, args);
         const m = /^<!([^!]*)!>/m.exec(stdout);
         if (m) {
-            return m[1];
+            return findLibLLDB(m[1]);
         }
     } catch {
         // ignore error just return undefined
     }
     return undefined;
+}
+
+async function findLibLLDB(pathHint: string): Promise<string | undefined> {
+    const stat = await fs.stat(pathHint);
+    if (stat.isFile()) {
+        return pathHint;
+    }
+
+    let libDir;
+    let pattern;
+    if (process.platform === "linux") {
+        libDir = path.join(pathHint, "lib");
+        pattern = /liblldb.*\.so.*/;
+    } else if (process.platform === "darwin") {
+        libDir = path.join(pathHint, "lib");
+        pattern = /liblldb\..*dylib|LLDB/;
+    } else if (process.platform === "win32") {
+        libDir = path.join(pathHint, "bin");
+        pattern = /liblldb\.dll/;
+    } else {
+        return pathHint;
+    }
+
+    for (const dir of [pathHint, libDir]) {
+        const file = await findFileByPattern(dir, pattern);
+        if (file) {
+            return path.join(dir, file);
+        }
+    }
+    return undefined;
+}
+
+async function findFileByPattern(path: string, pattern: RegExp): Promise<string | null> {
+    try {
+        const files = await fs.readdir(path);
+        for (const file of files) {
+            if (pattern.test(file)) {
+                return file;
+            }
+        }
+    } catch (err) {
+        // Ignore missing directories and such...
+    }
+    return null;
 }
