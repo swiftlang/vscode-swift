4356dabeabbfefd59137f438b392a83e7dbed1db
Adam Fowler <adamfowler71@gmail.com>
2022-01-13 16:46:01 +0000

    Add reset/clean commands (#111)
    * Add reset/clean commands
    
    Also add reset and resolve to dependency view
    
    * Fix up some naming
    
    * Add a resolve task after the reset

--- src/commands.ts
+++ src/commands.ts
@@ -119,6 +119,81 @@ export async function updateFolderDependencies(folderContext: FolderContext) {
 }
 
 /**
+ * Executes a {@link vscode.Task task} to delete all build artifacts.
+ */
+export async function cleanBuild(ctx: WorkspaceContext) {
+    const current = ctx.currentFolder;
+    if (!current) {
+        return;
+    }
+    await folderCleanBuild(current);
+}
+
+/**
+ * Run `swift package clean` inside a folder
+ * @param folderContext folder to run update inside
+ */
+export async function folderCleanBuild(folderContext: FolderContext) {
+    const workspaceContext = folderContext.workspaceContext;
+    const task = createSwiftTask(["package", "clean"], SwiftTaskProvider.cleanBuildName, {
+        scope: folderContext.folder,
+        presentationOptions: { reveal: vscode.TaskRevealKind.Silent },
+        group: vscode.TaskGroup.Clean,
+    });
+    workspaceContext.outputChannel.logStart("Clean Build ... ", folderContext.folder.name);
+    workspaceContext.statusItem.start(task);
+    try {
+        await executeTaskAndWait(task);
+        workspaceContext.outputChannel.logEnd("done.");
+    } catch (error) {
+        workspaceContext.outputChannel.logEnd(`${error}`);
+    }
+    workspaceContext.statusItem.end(task);
+}
+
+/**
+ * Executes a {@link vscode.Task task} to reset the complete cache/build directory.
+ */
+export async function resetPackage(ctx: WorkspaceContext) {
+    const current = ctx.currentFolder;
+    if (!current) {
+        return;
+    }
+    await folderResetPackage(current);
+}
+
+/**
+ * Run `swift package reset` inside a folder
+ * @param folderContext folder to run update inside
+ */
+export async function folderResetPackage(folderContext: FolderContext) {
+    const workspaceContext = folderContext.workspaceContext;
+    const task = createSwiftTask(["package", "reset"], "Reset Package Dependencies", {
+        scope: folderContext.folder,
+        presentationOptions: { reveal: vscode.TaskRevealKind.Silent },
+        group: vscode.TaskGroup.Clean,
+    });
+    workspaceContext.outputChannel.logStart("Reset Package ... ", folderContext.folder.name);
+    workspaceContext.statusItem.start(task);
+    try {
+        await executeTaskAndWait(task);
+        const resolveTask = createSwiftTask(
+            ["package", "resolve"],
+            SwiftTaskProvider.resolvePackageName,
+            {
+                scope: folderContext.folder,
+                presentationOptions: { reveal: vscode.TaskRevealKind.Silent },
+            }
+        );
+        await executeTaskAndWait(resolveTask);
+        workspaceContext.outputChannel.logEnd("done.");
+    } catch (error) {
+        workspaceContext.outputChannel.logEnd(`${error}`);
+    }
+    workspaceContext.statusItem.end(task);
+}
+
+/**
  * Registers this extension's commands in the given {@link vscode.ExtensionContext context}.
  */
 export function register(ctx: WorkspaceContext) {
@@ -128,6 +203,12 @@ export function register(ctx: WorkspaceContext) {
         }),
         vscode.commands.registerCommand("swift.updateDependencies", () => {
             updateDependencies(ctx);
+        }),
+        vscode.commands.registerCommand("swift.cleanBuild", () => {
+            cleanBuild(ctx);
+        }),
+        vscode.commands.registerCommand("swift.resetPackage", () => {
+            resetPackage(ctx);
         })
     );
 }
