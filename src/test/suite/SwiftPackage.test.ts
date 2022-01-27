bb8e996720adf90487d401d6886edc9ee7639eb0
Adam Fowler <adamfowler71@gmail.com>
2022-01-27 10:28:08 +0100

    Fix up code after rebase
    Include version of Steven Van Impe fixture code
    
    Co-Authored-By: Steven Van Impe <1496250+svanimpe@users.noreply.github.com>

--- src/test/suite/SwiftPackage.test.ts
+++ src/test/suite/SwiftPackage.test.ts
@@ -12,58 +12,34 @@
 //
 //===----------------------------------------------------------------------===//
 
-import * as path from 'path';
-import * as vscode from 'vscode';
-import * as assert from 'assert';
-import { SwiftPackage } from '../../SwiftPackage';
-import { integer } from 'vscode-languageclient';
+import * as assert from "assert";
+import { testAssetWorkspaceFolder } from "../fixtures";
+import { SwiftPackage } from "../../SwiftPackage";
 
-class TestWorkspaceFolder implements vscode.WorkspaceFolder {
-    constructor(
-        readonly uri: vscode.Uri
-    ) {}
-    get name(): string { return path.basename(this.uri.fsPath); }
-    get index(): integer { return 0; }
-}
-
-/**
-  * @returns the {@link vscode.Uri URI} of a resource in the **test** directory.
-  */
- export function testAssetUri(name: string): vscode.Uri {
-    return vscode.Uri.file(path.resolve(__dirname, '../../../assets/test', name));
-}
-
-/**
-  * @returns the {@link vscode.Uri URI} of a resource in the **test** directory.
-  */
- export function testAssetWorkspaceFolder(name: string): vscode.WorkspaceFolder {
-    return new TestWorkspaceFolder(testAssetUri(name));
-}
-
-suite('SwiftPackage Test Suite', () => {
-	test('No package', async () => {
-        let spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder('empty-folder'));
+suite("SwiftPackage Test Suite", () => {
+    test("No package", async () => {
+        const spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder("empty-folder"));
         assert.strictEqual(spmPackage.foundPackage, false);
     }).timeout(5000);
 
-	test('Invalid package', async () => {
-        let spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder('invalid-package'));
+    test("Invalid package", async () => {
+        const spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder("invalid-package"));
         assert.strictEqual(spmPackage.foundPackage, true);
         assert.strictEqual(spmPackage.isValid, false);
     }).timeout(5000);
 
-	test('Executable package', async () => {
-        let spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder('package1'));
-        assert.strictEqual(spmPackage.isValid, true)
+    test("Executable package", async () => {
+        const spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder("package1"));
+        assert.strictEqual(spmPackage.isValid, true);
         assert.strictEqual(spmPackage.executableProducts.length, 1);
         assert.strictEqual(spmPackage.executableProducts[0].name, "package1");
         assert.strictEqual(spmPackage.dependencies.length, 1);
         assert.strictEqual(spmPackage.targets.length, 2);
     }).timeout(5000);
 
-	test('Library package', async () => {
-        let spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder('package2'));
-        assert.strictEqual(spmPackage.isValid, true)
+    test("Library package", async () => {
+        const spmPackage = await SwiftPackage.create(testAssetWorkspaceFolder("package2"));
+        assert.strictEqual(spmPackage.isValid, true);
         assert.strictEqual(spmPackage.libraryProducts.length, 1);
         assert.strictEqual(spmPackage.libraryProducts[0].name, "package2");
         assert.strictEqual(spmPackage.dependencies.length, 0);
--- src/test/suite/WorkspaceContext.test.ts
+++ src/test/suite/WorkspaceContext.test.ts
@@ -12,55 +12,31 @@
 //
 //===----------------------------------------------------------------------===//
 
-import * as path from 'path';
-import * as vscode from 'vscode';
-import * as assert from 'assert';
-import { integer } from 'vscode-languageclient';
-import { SwiftExtensionContext, WorkspaceContext } from '../../WorkspaceContext';
-
-class TestWorkspaceFolder implements vscode.WorkspaceFolder {
-    constructor(
-        readonly uri: vscode.Uri
-    ) {}
-    get name(): string { return path.basename(this.uri.fsPath); }
-    get index(): integer { return 0; }
-}
+import * as assert from "assert";
+import { testAssetWorkspaceFolder } from "../fixtures";
+import { FolderEvent, SwiftExtensionContext, WorkspaceContext } from "../../WorkspaceContext";
 
 class TestExtensionContext implements SwiftExtensionContext {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     readonly subscriptions: { dispose(): any }[] = [];
 }
 
-/**
-  * @returns the {@link vscode.Uri URI} of a resource in the **test** directory.
-  */
- export function testAssetUri(name: string): vscode.Uri {
-    return vscode.Uri.file(path.resolve(__dirname, '../../../assets/test', name));
-}
-
-/**
-  * @returns the {@link vscode.Uri URI} of a resource in the **test** directory.
-  */
- export function testAssetWorkspaceFolder(name: string): vscode.WorkspaceFolder {
-    return new TestWorkspaceFolder(testAssetUri(name));
-}
-
-suite('WorkspaceContext Test Suite', () => {
-	test('Add/Remove', async () => {
+suite("WorkspaceContext Test Suite", () => {
+    test("Add/Remove", async () => {
         let count = 0;
         const workspaceContext = new WorkspaceContext(new TestExtensionContext());
-        workspaceContext.observerFolders((folder, operation) => {
-            assert.strictEqual(folder.swiftPackage.name, 'package1');
+        workspaceContext.observeFolders((folder, operation) => {
+            assert.strictEqual(folder.swiftPackage.name, "package1");
             switch (operation) {
-            case 'add':
-                count++;
-                break;
-            case 'remove':
-                count--;
-                break;
+                case FolderEvent.add:
+                    count++;
+                    break;
+                case FolderEvent.remove:
+                    count--;
+                    break;
             }
         });
-        const packageFolder = testAssetWorkspaceFolder('package1');
+        const packageFolder = testAssetWorkspaceFolder("package1");
         await workspaceContext.addFolder(packageFolder);
         await workspaceContext.removeFolder(packageFolder);
         assert.strictEqual(count, 0);
