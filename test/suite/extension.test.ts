637e0aafd99a84b5c018dbb46693c4fb4c24aa4b
Adam Fowler <adamfowler71@gmail.com>
2022-04-11 17:41:45 +0100

    Move temporary file test to extension.test.ts

---
+++ test/suite/extension.test.ts
@@ -0,0 +1,43 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VSCode Swift open source project
+//
+// Copyright (c) 2021-2022 the VSCode Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VSCode Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import * as vscode from "vscode";
+import * as assert from "assert";
+import * as fs from "fs/promises";
+import * as swiftExtension from "../../src/extension";
+import { WorkspaceContext } from "../../src/WorkspaceContext";
+
+suite("Extension Test Suite", () => {
+    let workspaceContext: WorkspaceContext;
+
+    suiteSetup(async () => {
+        const ext = vscode.extensions.getExtension<swiftExtension.Api>("sswg.swift-lang")!;
+        const api = await ext.activate();
+        workspaceContext = api.workspaceContext;
+    });
+
+    suite("Temporary Folder Test Suite", () => {
+        test("Create/Delete File", async () => {
+            const fileContents = "Test file";
+            //const tempFolder = await TemporaryFolder.create();
+            const fileName = workspaceContext.tempFolder.filename("test");
+            assert.doesNotThrow(async () => await fs.writeFile(fileName, fileContents));
+            assert.doesNotThrow(async () => {
+                const contents = await fs.readFile(fileName, "utf8");
+                assert.strictEqual(contents, fileContents);
+            });
+            assert.doesNotThrow(async () => await fs.rm(fileName));
+        });
+    });
+});
