553fff02dd95a752c74eee32280134cd8e466b5f
YR Chen <stevapple@icloud.com>
2022-04-14 19:56:18 +0800

    Introduce `getExecutableName` (#252)

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -101,12 +101,21 @@ export async function execSwift(
 }
 
 /**
+ * Get the file name of executable
+ *
+ * @param exe name of executable to return
+ */
+export function getExecutableName(exe: string): string {
+    return process.platform === "win32" ? `${exe}.exe` : exe;
+}
+
+/**
  * Get path to swift executable, or executable in swift bin folder
  *
  * @param exe name of executable to return
  */
 export function getSwiftExecutable(exe = "swift"): string {
-    return path.join(configuration.path, exe);
+    return path.join(configuration.path, getExecutableName(exe));
 }
 
 /**
