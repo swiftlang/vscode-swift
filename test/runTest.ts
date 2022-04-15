073dbcd64cbfcb591f441e0b85061dae552f5c4d
Adam Fowler <adamfowler71@gmail.com>
2022-04-15 08:38:39 +0100

    CI: Run tests on swiftserver (#216)
    * Setup docker for ci.swiftserver.group
    
    * Fix Package resolve v2 test prior to swift:5.6
    
    * Make packages 5.4 so 5.4 tests work
    
    * Run lint in docker PR test
    
    * Don't disable extensions in tests
    
    * Fix runTests to work from command line
    
    * Install vadimcn.vscode-lldb
    
    * Install code-lldb before running tests
    
    * Update runTest.ts
    
    * Add non-root user to run vscode
    
    * Set user home folder

--- test/runTest.ts
+++ test/runTest.ts
@@ -12,25 +12,49 @@
 //
 //===----------------------------------------------------------------------===//
 
+import * as cp from "child_process";
 import * as path from "path";
-
-import { runTests } from "@vscode/test-electron";
+import {
+    runTests,
+    downloadAndUnzipVSCode,
+    resolveCliPathFromVSCodeExecutablePath,
+} from "@vscode/test-electron";
 
 async function main() {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
-        const extensionDevelopmentPath = path.resolve(__dirname, "../");
+        const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
         // The path to test runner
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
+        const vscodeExecutablePath = await downloadAndUnzipVSCode();
+        const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+        // Use cp.spawn / cp.exec for custom setup
+        console.log(`${cliPath} --install-extension vadimcn.vscode-lldb`);
+        const { stdout, stderr } = cp.spawnSync(
+            cliPath,
+            ["--install-extension", "vadimcn.vscode-lldb"],
+            {
+                encoding: "utf-8",
+                stdio: "inherit",
+            }
+        );
+        console.log(stdout);
+        console.log(stderr);
+
         // Download VS Code, unzip it and run the integration test
         await runTests({
             extensionDevelopmentPath,
             extensionTestsPath,
-            launchArgs: ["--disable-extensions"],
+            launchArgs: [
+                // Already start in the fixtures dir because we lose debugger connection
+                // once we re-open a different folder due to window reloading
+                path.join(extensionDevelopmentPath, "assets/test"),
+            ],
         });
     } catch (err) {
         console.error("Failed to run tests");
