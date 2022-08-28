082fe5b5a72dedbc75ecef496e14cd9e73819455
Adam Fowler <adamfowler71@gmail.com>
2022-08-28 18:27:33 +0100

    Store hash of Package.resolved (#401)

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -426,3 +426,22 @@ export function filterArguments(args: string[], filter: ArgumentFilter[]): strin
     }
     return filteredArguments;
 }
+
+/**
+ * String hashing function taken from https://stackoverflow.com/a/52171480/7831758
+ * @param str String to hash
+ * @param seed Seed for hash function
+ * @returns Hash of string
+ */
+export function hashString(str: string, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed,
+        h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
