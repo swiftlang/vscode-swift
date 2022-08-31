6633e9786a794f78a3b27444500f78b790cfc207
Adam Fowler <adamfowler71@gmail.com>
2022-08-31 18:20:48 +0100

    Add allowWritingToPackageDirectory to swift plugin definition

--- src/SwiftPluginTaskProvider.ts
+++ src/SwiftPluginTaskProvider.ts
@@ -74,9 +74,13 @@ export class SwiftPluginTaskProvider implements vscode.TaskProvider {
         // Reusing the task parameter doesn't seem to work.
         const swift = getSwiftExecutable();
         const sandboxArg = task.definition.disableSandbox ? ["--disable-sandbox"] : [];
+        const writingToPackageArg = task.definition.allowWritingToPackageDirectory
+            ? ["--allow-writing-to-package-directory"]
+            : [];
         let swiftArgs = [
             "package",
             ...sandboxArg,
+            ...writingToPackageArg,
             task.definition.command,
             ...task.definition.args,
         ];
