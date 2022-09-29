56a972fd694c5ffa4b90fdca43e30c28049a3e52
Adam Fowler <adamfowler71@gmail.com>
2022-09-29 11:22:56 +0100

    Script for tagging releasing

---
+++ scripts/tag_release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the VSCode Swift open source project
+##
+## Copyright (c) 2021 the VSCode Swift project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of VSCode Swift project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+tag=$1
+
+if [[ -z $tag ]]; then
+    echo "tag_release.sh requires a tag"
+    exit -1
+fi
+
+git tag "$tag"
+git push upstream "$tag"
