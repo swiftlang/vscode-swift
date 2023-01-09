0630fa8e9a7fdc33b18737d393a5673e38c85837
Adam Fowler <adamfowler71@gmail.com>
2023-01-09 11:11:50 +0000

    Added SourceKitLSPErrorHandler
    Language client crash handler that asks if you want to restart the server again after so many crashes

--- src/sourcekit-lsp/LanguageClientManager.ts
+++ src/sourcekit-lsp/LanguageClientManager.ts
@@ -436,6 +436,7 @@ export class LanguageClientManager {
                     return result;
                 },
             },
+            errorHandler: new SourceKitLSPErrorHandler(5),
         };
 
         return new langclient.LanguageClient(
@@ -490,6 +491,71 @@ export class LanguageClientManager {
         this.cancellationToken = new vscode.CancellationTokenSource();
 
         return this.clientReadyPromise;
+    }
+}
+
+/**
+ * SourceKit-LSP error handler. Copy of the default error handler, except it includes
+ * an error message that asks if you want to restart the sourcekit-lsp server again
+ * after so many crashes
+ */
+class SourceKitLSPErrorHandler implements langclient.ErrorHandler {
+    private restarts: number[];
+
+    constructor(private maxRestartCount: number) {
+        this.restarts = [];
+    }
+    /**
+     * An error has occurred while writing or reading from the connection.
+     *
+     * @param error - the error received
+     * @param message - the message to be delivered to the server if know.
+     * @param count - a count indicating how often an error is received. Will
+     *  be reset if a message got successfully send or received.
+     */
+    error(
+        error: Error,
+        message: langclient.Message | undefined,
+        count: number | undefined
+    ): langclient.ErrorHandlerResult | Promise<langclient.ErrorHandlerResult> {
+        if (count && count <= 3) {
+            return { action: langclient.ErrorAction.Continue };
+        }
+        return { action: langclient.ErrorAction.Shutdown };
+    }
+    /**
+     * The connection to the server got closed.
+     */
+    closed(): langclient.CloseHandlerResult | Promise<langclient.CloseHandlerResult> {
+        this.restarts.push(Date.now());
+        if (this.restarts.length <= this.maxRestartCount) {
+            return { action: langclient.CloseAction.Restart };
+        } else {
+            const diff = this.restarts[this.restarts.length - 1] - this.restarts[0];
+            if (diff <= 3 * 60 * 1000) {
+                return new Promise<langclient.CloseHandlerResult>(resolve => {
+                    vscode.window
+                        .showErrorMessage(
+                            `The SourceKit-LSP server crashed ${
+                                this.maxRestartCount + 1
+                            } times in the last 3 minutes. See the output for more information. Do you want to restart it again.`,
+                            "Yes",
+                            "No"
+                        )
+                        .then(result => {
+                            if (result === "Yes") {
+                                this.restarts = [];
+                                resolve({ action: langclient.CloseAction.Restart });
+                            } else {
+                                resolve({ action: langclient.CloseAction.DoNotRestart });
+                            }
+                        });
+                });
+            } else {
+                this.restarts.shift();
+                return { action: langclient.CloseAction.Restart };
+            }
+        }
     }
 }
 
