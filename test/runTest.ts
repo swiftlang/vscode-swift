ceea97a6745826d3df1b627acdce8bf5e3a7f111
Adam Fowler <adamfowler71@gmail.com>
2023-03-16 18:39:00 +0000

    No need to install CodeLLDB for test anymore

--- test/runTest.ts
+++ test/runTest.ts
@@ -12,13 +12,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-import * as cp from "child_process";
 import * as path from "path";
-import {
-    runTests,
-    downloadAndUnzipVSCode,
-    resolveCliPathFromVSCodeExecutablePath,
-} from "@vscode/test-electron";
+import { runTests } from "@vscode/test-electron";
 
 async function main() {
     try {
@@ -29,22 +24,6 @@ async function main() {
         // The path to test runner
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-
-        const vscodeExecutablePath = await downloadAndUnzipVSCode();
-        const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
-
-        // Use cp.spawn / cp.exec for custom setup
-        console.log(`${cliPath} --install-extension vadimcn.vscode-lldb`);
-        const { stdout, stderr } = cp.spawnSync(
-            cliPath,
-            ["--install-extension", "vadimcn.vscode-lldb"],
-            {
-                encoding: "utf-8",
-                stdio: "inherit",
-            }
-        );
-        console.log(stdout);
-        console.log(stderr);
 
         // Download VS Code, unzip it and run the integration test
         await runTests({
