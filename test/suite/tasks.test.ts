71e7ed4b9e7d9f931a44990d24f7c0a14e91ec8c
Adam Fowler <adamfowler71@gmail.com>
2023-04-19 17:17:45 +0100

    Fix tests

--- test/suite/tasks.test.ts
+++ test/suite/tasks.test.ts
@@ -20,7 +20,18 @@ import { WorkspaceContext } from "../../src/WorkspaceContext";
 import { TaskQueue } from "../../src/TaskQueue";
 
 suite("Tasks Test Suite", () => {
-    const taskManager = new TaskManager();
+    let workspaceContext: WorkspaceContext;
+    let taskManager: TaskManager;
+
+    suiteSetup(async () => {
+        workspaceContext = await WorkspaceContext.create();
+        taskManager = new TaskManager(workspaceContext);
+    });
+
+    suiteTeardown(async () => {
+        taskManager.dispose();
+        workspaceContext.dispose();
+    });
 
     suite("TaskManager", () => {
         // check running task will return expected value
