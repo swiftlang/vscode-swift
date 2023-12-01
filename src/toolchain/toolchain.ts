9280122c090d571e31051db5b4b3618454c344f2
Adam Fowler <adamfowler71@gmail.com>
2023-12-01 10:12:07 +0000

    Update toolchain.ts

--- src/toolchain/toolchain.ts
+++ src/toolchain/toolchain.ts
@@ -2,7 +2,7 @@
 //
 // This source file is part of the VSCode Swift open source project
 //
-// Copyright (c) 2021 the VSCode Swift project authors
+// Copyright (c) 2021-2023 the VSCode Swift project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
