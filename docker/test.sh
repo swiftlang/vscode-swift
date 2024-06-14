c40200b9283e1ebef14cef6a02849acaf3f48a2d
Paul LeMarquand <plemarquand@apple.com>
2024-06-14 11:39:31 -0400

    Run tests with code coverage (#894)
    To help identify poorly tested areas of the extension, run the tests
    with code coverage. Coverage is reported in the terminal as text as well
    as stored in the `/coverage` directory as HTML.
    
    In CI results can be seen in the terminal at the end of the test run.
    Locally when running tests in the docker containers HTML coverage
    results will appear in the `/coverage` folder after the run completes.

--- docker/test.sh
+++ docker/test.sh
@@ -1,2 +1,15 @@
-(xvfb-run -a npm test; echo $? > exitcode) | grep -Ev "Failed to connect to the bus|GPU stall due to ReadPixels"
+set -ex
+
+current_directory=$(pwd)
+
+mkdir /tmp/code
+cp -r ./ /tmp/code/
+cd /tmp/code
+
+npm ci
+npm run lint
+npm run format
+npm run package
+
+(xvfb-run -a npm run coverage; echo $? > exitcode) | grep -Ev "Failed to connect to the bus|GPU stall due to ReadPixels" && rm -rf "${current_directory}/coverage" && (cp -R ./coverage $current_directory || true)
 exit $(<exitcode)
