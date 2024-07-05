a9aa6bcb056ae548309b2151299c8301725fa189
Paul LeMarquand <plemarquand@apple.com>
2024-07-05 08:21:46 -0400

    Use @vscode/test-cli to run tests (#949)
    Migrates to the new @vscode/test-cli package to download the appropriate
    VS Code and run the tests. This streamlines the test setup code, and
    should be a more reliable way to run the tests as VS Code evolves.
    
    Hopefully this also helps with the occasional SIGSEGV errors that happen
    in CI on startup.

---
+++ .vscode-test.js
@@ -0,0 +1,50 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2024 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+const { defineConfig } = require("@vscode/test-cli");
+
+const isCIBuild = process.env["CI"] === "1";
+const isFastTestRun = process.env["FAST_TEST_RUN"] === "1";
+
+// "env" in launch.json doesn't seem to work with vscode-test
+const isDebugRun = !(process.env["_"] ?? "").endsWith("node_modules/.bin/vscode-test");
+
+// so tests don't timeout when a breakpoint is hit
+const timeout = isDebugRun ? Number.MAX_SAFE_INTEGER : 3000
+
+module.exports = defineConfig({
+    tests: [
+        {
+            label: "integrationTests",
+            files: ["out/test/suite/**/*.test.js"],
+            version: "stable",
+            workspaceFolder: "./assets/test",
+            mocha: {
+                ui: "tdd",
+                color: true,
+                timeout,
+                forbidOnly: isCIBuild,
+                grep: isFastTestRun ? "@slow" : undefined,
+                invert: isFastTestRun,
+            },
+            installExtensions: ["vadimcn.vscode-lldb"],
+            reuseMachineInstall: !isCIBuild,
+        },
+        // you can specify additional test configurations, too
+    ],
+    coverage: {
+        include: ["**/src/**"],
+        reporter: ["text", "html"],
+    },
+});
