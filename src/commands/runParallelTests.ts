2cb14cf3b76eba9c332fbe4bb69b1a0032cc96b9
Paul LeMarquand <plemarquand@apple.com>
2024-09-11 09:44:24 -0400

    Add licence header

--- src/commands/runParallelTests.ts
+++ src/commands/runParallelTests.ts
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2021-2024 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import * as vscode from "vscode";
 import { TestKind } from "../TestExplorer/TestKind";
 import { WorkspaceContext } from "../WorkspaceContext";
