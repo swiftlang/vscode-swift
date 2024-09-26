29cf367172841e8de177369cd07bd269aae7fb44
Paul LeMarquand <plemarquand@apple.com>
2024-09-26 09:45:01 -0400

    Only copy required files into Docker work directory
    Use `rsync` to exclude copying unnecessary files in to the working
    directory. This mainly helps speed up running the containers during
    local development since the developer may already have `node_modules`,
    `.build`, and `.vscode-test`, all of which can be quite large.

--- docker/test.sh
+++ docker/test.sh
@@ -3,7 +3,8 @@ set -ex
 current_directory=$(pwd)
 
 mkdir /tmp/code
-cp -r ./ /tmp/code/
+# Add the -v flag to see what is getting copied in to the working folder
+rsync -a --exclude 'node_modules' --exclude '.git' --exclude '.vscode-test' --exclude '.build' ./ /tmp/code/
 cd /tmp/code
 
 npm ci
