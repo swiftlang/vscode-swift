d184a334616f34a2eb2cbb79f16be5c0e74e9cac
award999 <award999@apple.com>
2024-10-01 14:40:26 -0400

    Add a script for running our nightly suite of tests (#1107)
    * Add a script for running our nightly suite of tests
    
    Right now the only difference to CI is we run @slow plus CI tests
    
    * Unify into one script
    
    * Address comment

--- docker/test-ci.sh
+++ docker/test-ci.sh
@@ -2,9 +2,14 @@
 
 const { execSync } = require('child_process');
 
+// Default to 'ci' if no argument is provided
+const job = (process.argv[2] || 'ci')
+
 // Default to 'main' if no argument is provided
-const swiftVersion = (process.argv[2] || 'main').replace(/\./g, '');
-const command = `docker compose -f docker/docker-compose.yaml -f docker/docker-compose.*.${swiftVersion}.yaml -p swift-vscode-${swiftVersion}-prb run --rm test`;
+const swiftVersion = (process.argv[3] || 'main').replace(/\./g, '');
+
+const baseConfigFile = job === 'ci' ? 'docker-compose.yaml' : 'docker-compose-nightly.yaml'
+const command = `docker compose -f docker/${baseConfigFile} -f docker/docker-compose.*.${swiftVersion}.yaml -p swift-vscode-${swiftVersion}-prb run --rm test`;
 
 console.log(`Running: ${command}`);
 execSync(command, { stdio: 'inherit' });
