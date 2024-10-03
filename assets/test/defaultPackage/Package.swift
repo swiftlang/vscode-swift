df7b15715c1a0c0f95d301be338269b028bb0d0a
Adam Ward <award999@apple.com>
2024-10-03 15:31:01 -0400

    Add test snippet source

--- assets/test/defaultPackage/Package.swift
+++ assets/test/defaultPackage/Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "defaultPackage",
+    products: [
+        .library(
+            name: "PackageLib",
+            targets: ["PackageLib"]),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
