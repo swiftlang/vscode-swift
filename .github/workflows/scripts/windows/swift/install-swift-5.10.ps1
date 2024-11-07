7578469173c761f264d430d2f6951fe94227b413
Adam Ward <award999@apple.com>
2024-11-07 10:48:01 -0500

    Point to branch in forked workflows repo

--- .github/workflows/scripts/windows/swift/install-swift-5.10.ps1
+++
@@ -1,6 +0,0 @@
-. $PSScriptRoot\install-swift.ps1
-
-$SWIFT='https://download.swift.org/swift-5.10.1-release/windows10/swift-5.10.1-RELEASE/swift-5.10.1-RELEASE-windows10.exe'
-$SWIFT_SHA256='3027762138ACFA1BBE3050FF6613BBE754332E84C9EFA5C23984646009297286'
-
-Install-Swift -Url $SWIFT -Sha256 $SWIFT_SHA256
--- .github/workflows/scripts/windows/swift/install-swift-6.0.ps1
+++
@@ -1,6 +0,0 @@
-. $PSScriptRoot\install-swift.ps1
-
-$SWIFT='https://download.swift.org/swift-6.0.2-release/windows10/swift-6.0.2-RELEASE/swift-6.0.2-RELEASE-windows10.exe'
-$SWIFT_SHA256='516FE8E64713BD92F03C01E5198011B74A27F8C1C88627607A2F421718636126'
-
-Install-Swift -Url $SWIFT -Sha256 $SWIFT_SHA256
