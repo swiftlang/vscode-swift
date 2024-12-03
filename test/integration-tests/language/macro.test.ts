5072ed54c1221a89eba28783cc24810a835e6694
Michael (SPG) Weng <mweng@apple.com>
2024-12-03 17:12:17 -0500

    Adjust to only test for expand macro, as this is the only request that needed custom support in the extension.

--- test/integration-tests/language/macro.test.ts
+++ test/integration-tests/language/macro.test.ts
@@ -19,7 +19,7 @@ import { LanguageClientManager } from "../../../src/sourcekit-lsp/LanguageClient
 import { WorkspaceContext } from "../../../src/WorkspaceContext";
 import { testAssetUri } from "../../fixtures";
 import { FolderContext } from "../../../src/FolderContext";
-import { waitForEndTaskProcess, waitForNoRunningTasks } from "../../utilities";
+import { executeTaskAndWaitForResult, waitForNoRunningTasks } from "../../utilities";
 import { getBuildAllTask, SwiftTask } from "../../../src/tasks/SwiftTaskProvider";
 import { Version } from "../../../src/utilities/version";
 import { activateExtensionForSuite, folderInRootWorkspace } from "../utilities/testutilities";
@@ -48,8 +48,8 @@ suite("Integration, Macros Functionality Support with Sourcekit-lsp", function (
     activateExtensionForSuite({
         async setup(ctx) {
             workspaceContext = ctx;
-            // Macro support in Swift started from 5.9, related support in Sourcekit-lsp started from 5.10
-            if (workspaceContext.swiftVersion.isLessThan(new Version(5, 10, 0))) {
+            // Expand Macro support in Swift started from 6.1
+            if (workspaceContext.swiftVersion.isLessThan(new Version(6, 1, 0))) {
                 this.skip();
             }
 
@@ -58,10 +58,8 @@ suite("Integration, Macros Functionality Support with Sourcekit-lsp", function (
             folderContext = await folderInRootWorkspace("swift-macro", workspaceContext);
             await workspaceContext.focusFolder(folderContext);
             const tasks = (await getBuildAllTask(folderContext)) as SwiftTask;
-            const exitPromise = waitForEndTaskProcess(tasks);
-            await vscode.tasks.executeTask(tasks);
-            const exitCode = await exitPromise;
-            expect(exitCode).to.equal(0);
+            const { exitCode, output } = await executeTaskAndWaitForResult(tasks);
+            expect(exitCode, `${output}`).to.equal(0);
 
             // Ensure lsp client is ready
             clientManager = workspaceContext.languageClientManager;
@@ -70,10 +68,10 @@ suite("Integration, Macros Functionality Support with Sourcekit-lsp", function (
         },
     });
 
-    test("Inline/Expand Macro", async function () {
+    test("Expand Macro", async function () {
         // Focus on the file of interest
         const uri = testAssetUri("swift-macro/Sources/swift-macroClient/main.swift");
-        const editor = await vscode.window.showTextDocument(uri);
+        await vscode.window.showTextDocument(uri);
 
         // Beginning of macro, #
         const position = new vscode.Position(5, 21);
@@ -88,85 +86,46 @@ suite("Integration, Macros Functionality Support with Sourcekit-lsp", function (
             range
         );
 
-        // Log and assert the code actions
-        expect(codeActions).to.be.an("array");
-        // Expand Macro action requires Swift 6.1
-        // Inline Macro action requires Swift 5.10, anything less than 5.10 is skipped in suite set up
-        let expectedLength;
-        if (workspaceContext.swiftVersion.isGreaterThanOrEqual(new Version(6, 1, 0))) {
-            expectedLength = 2;
-        } else {
-            expectedLength = 1;
-        }
-        expect(
-            codeActions.length,
-            `Unexpected codeActions length. Received actions: ${JSON.stringify(codeActions, null, 2)}`
-        ).to.be.equal(expectedLength);
-
         const expectedMacro = '(a + b, "a + b")';
-        // Loop through the code actions and execute them based on the command id
-        for (const action of codeActions) {
-            expect(action.command).is.not.undefined;
-            const command = action.command!;
-            expect(command.arguments).is.not.undefined;
-            const commandArgs = command.arguments!;
-            // The id for the action is not clear, the title is "inline macro"
-            if (command.command === "semantic.refactor.command") {
-                // Run inline macro action
-                await vscode.commands.executeCommand(command.command, ...commandArgs);
 
-                // Assert that the macro was inlined correctly
-                const endPosition = new vscode.Position(5, 37);
-                const inlineRange = new vscode.Range(position, endPosition);
-                const updatedText = editor.document.getText(inlineRange);
-                expect(updatedText).to.equal(expectedMacro);
+        // Find the "expand.macro.command" action
+        const expandMacroAction = codeActions.find(
+            action => action.command?.command === "expand.macro.command"
+        );
 
-                // Ensure we are refocusing on the text document for the undo step
-                await vscode.window.showTextDocument(uri);
+        // Assert that the expand macro command is available
+        expect(expandMacroAction).is.not.undefined;
 
-                // We need to undo the inline macro or subsequent action will fail
-                await vscode.commands.executeCommand("undo");
-            } else if (command.command === "expand.macro.command") {
-                // Set up a promise that resolves when the expected document is opened
-                const expandedMacroUriPromise = new Promise<vscode.TextDocument>(
-                    (resolve, reject) => {
-                        const disposable = vscode.workspace.onDidOpenTextDocument(
-                            openedDocument => {
-                                if (openedDocument.uri.scheme === "sourcekit-lsp") {
-                                    disposable.dispose(); // Stop listening once we find the desired document
-                                    resolve(openedDocument);
-                                }
-                            }
-                        );
+        // Set up a promise that resolves when the expected document is opened
+        const expandedMacroUriPromise = new Promise<vscode.TextDocument>((resolve, reject) => {
+            const disposable = vscode.workspace.onDidOpenTextDocument(openedDocument => {
+                if (openedDocument.uri.scheme === "sourcekit-lsp") {
+                    disposable.dispose(); // Stop listening once we find the desired document
+                    resolve(openedDocument);
+                }
+            });
 
-                        // Set a timeout to reject the promise if the document is not found
-                        setTimeout(() => {
-                            disposable.dispose();
-                            reject(
-                                new Error(
-                                    "Timed out waiting for sourcekit-lsp document to be opened."
-                                )
-                            );
-                        }, 10000); // Wait up to 10 seconds for the document
-                    }
-                );
+            // Set a timeout to reject the promise if the document is not found
+            setTimeout(() => {
+                disposable.dispose();
+                reject(new Error("Timed out waiting for sourcekit-lsp document to be opened."));
+            }, 10000); // Wait up to 10 seconds for the document
+        });
 
-                // Run expand macro action
-                await vscode.commands.executeCommand(command.command, ...commandArgs);
+        // Run expand macro action
+        const command = expandMacroAction!.command!;
+        expect(command.arguments).is.not.undefined;
+        const commandArgs = command.arguments!;
+        await vscode.commands.executeCommand(command.command, ...commandArgs);
 
-                // Wait for the expanded macro document to be opened
-                const referenceDocument = await expandedMacroUriPromise;
+        // Wait for the expanded macro document to be opened
+        const referenceDocument = await expandedMacroUriPromise;
 
-                // Verify that the reference document was successfully opened
-                expect(referenceDocument).to.not.be.undefined;
+        // Verify that the reference document was successfully opened
+        expect(referenceDocument).to.not.be.undefined;
 
-                // Assert that the content contains the expected result
-                const content = referenceDocument.getText();
-                expect(content).to.include(expectedMacro);
-            } else {
-                // New action got added, we need to add a new test case if this is hit.
-                expect.fail();
-            }
-        }
+        // Assert that the content contains the expected result
+        const content = referenceDocument.getText();
+        expect(content).to.include(expectedMacro);
     });
 });
