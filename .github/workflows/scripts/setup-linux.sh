cf37af5f48f778d4a5758cccd934eb012a018813
award999 <award999@apple.com>
2024-12-10 14:07:59 -0500

    Setup a nightly build job (#1251)
    * Setup a nightly build job
    
    * Renaming workflow file to "test.yml" so we can share the setup, using
      the event_name to decide if full test matrix should be run or not
    * Run insiders nightly
    
    Issue: #934
    
    * Eval for linux command
    
    * Add linux setup to script
    
    * Move node/nvm env vars to setup script
    
    * Split jobs into separate files
    
    Not easy to share across extended workflows and would show a bunch of
    skipped jobs in the PR so will have as separate files, sharing scripts
    
    * Fix soundness checks
    
    * Only run current release against insiders
    
    * Verify nightly build if PR changes that file
    
    * Don't run nightly-6.0 in CI
    
    With a nightly job in place, we don't want unreleased changes to break
    our CI
    
    * Fix review comment

---
+++ .github/workflows/scripts/setup-linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the VS Code Swift open source project
+##
+## Copyright (c) 2024 the VS Code Swift project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+export NODE_VERSION=v18.19.0
+export NODE_PATH=/usr/local/nvm/versions/node/v18.19.0/bin
+export NVM_DIR=/usr/local/nvm
+
+apt-get update && apt-get install -y rsync curl gpg libasound2 libgbm1 libgtk-3-0 libnss3 xvfb build-essential
+mkdir -p $NVM_DIR
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+/bin/bash -c "source $NVM_DIR/nvm.sh && nvm install $NODE_VERSION"
+echo "$NODE_PATH" >> "$GITHUB_PATH"
