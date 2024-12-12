25ee8432f2082e618701392790ca5fdadbf65201
Matthew Bastien <matthew_bastien@apple.com>
2024-12-12 12:25:23 -0500

    Add a custom build for swift-docc-render (#1229)
    Co-authored-by: Adam Ward <award999@apple.com>

---
+++ scripts/update_swift_docc_render.ts
@@ -0,0 +1,120 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2024 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+/* eslint-disable no-console */
+
+import simpleGit, { ResetMode } from "simple-git";
+import { spawn } from "child_process";
+import { stat, mkdtemp, mkdir, rm, readdir } from "fs/promises";
+import * as path from "path";
+import { tmpdir } from "os";
+import * as semver from "semver";
+
+function checkNodeVersion() {
+    const nodeVersion = semver.parse(process.versions.node);
+    if (nodeVersion === null) {
+        throw new Error(
+            "Unable to determine the version of NodeJS that this script is running under."
+        );
+    }
+    if (!semver.satisfies(nodeVersion, "18")) {
+        throw new Error(
+            `Cannot build swift-docc-render with NodeJS v${nodeVersion.raw}. Please install and use NodeJS v18.`
+        );
+    }
+}
+
+async function cloneSwiftDocCRender(buildDirectory: string): Promise<string> {
+    // Clone swift-docc-render
+    const swiftDocCRenderDirectory = path.join(buildDirectory, "swift-docc-render");
+    const git = simpleGit({ baseDir: buildDirectory });
+    console.log("> git clone https://github.com/swiftlang/swift-docc-render.git");
+    const revision = "10b097153d89d7bfc2dd400b47181a782a0cfaa0";
+    await git.clone("https://github.com/swiftlang/swift-docc-render.git", swiftDocCRenderDirectory);
+    await git.cwd(swiftDocCRenderDirectory);
+    await git.reset(ResetMode.HARD, [revision]);
+    // Apply our patches to swift-docc-render
+    const patches = (
+        await readdir(path.join(__dirname, "patches", "swift-docc-render"), {
+            withFileTypes: true,
+        })
+    )
+        .filter(entity => entity.isFile() && entity.name.endsWith(".patch"))
+        .map(entity => path.join(entity.path, entity.name))
+        .sort();
+    console.log("> git apply \\\n" + patches.map(e => "    " + e).join(" \\\n"));
+    await git.applyPatch(patches);
+    return swiftDocCRenderDirectory;
+}
+
+async function exec(
+    command: string,
+    args: string[],
+    options: { cwd?: string; env?: { [key: string]: string } } = {}
+): Promise<void> {
+    let logMessage = "> " + command;
+    if (args.length > 0) {
+        logMessage += " " + args.join(" ");
+    }
+    console.log(logMessage + "\n");
+    return new Promise<void>((resolve, reject) => {
+        const childProcess = spawn(command, args, { stdio: "inherit", ...options });
+        childProcess.once("error", reject);
+        childProcess.once("close", (code, signal) => {
+            if (signal !== null) {
+                reject(new Error(`Process exited due to signal '${signal}'`));
+            } else if (code !== 0) {
+                reject(new Error(`Process exited with code ${code}`));
+            } else {
+                resolve();
+            }
+            console.log("");
+        });
+    });
+}
+
+(async () => {
+    const outputDirectory = path.join(__dirname, "..", "assets", "swift-docc-render");
+    if (process.argv.includes("postinstall")) {
+        try {
+            await stat(outputDirectory);
+            console.log(`${outputDirectory} exists, skipping build.`);
+            return;
+        } catch {
+            // Proceed with creating
+        }
+    }
+    checkNodeVersion();
+    await rm(outputDirectory, { force: true, recursive: true });
+    await mkdir(outputDirectory, { recursive: true });
+    const buildDirectory = await mkdtemp(path.join(tmpdir(), "update_swift_docc_render"));
+    try {
+        const swiftDocCRenderDirectory = await cloneSwiftDocCRender(buildDirectory);
+        await exec("npm", ["install"], { cwd: swiftDocCRenderDirectory });
+        await exec("npx", ["vue-cli-service", "build", "--dest", outputDirectory], {
+            cwd: swiftDocCRenderDirectory,
+            env: {
+                ...process.env,
+                VUE_APP_TARGET: "ide",
+            },
+        });
+    } finally {
+        await rm(buildDirectory, { force: true, recursive: true }).catch(error => {
+            console.error(`Failed to remove temporary directory '${buildDirectory}'`);
+            console.error(error);
+        });
+    }
+})().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
