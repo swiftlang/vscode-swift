64c38ac01474bade3e8bf67cce6c4d99f606db9f
Adam Ward <award999@apple.com>
2025-01-30 08:55:25 -0500

    Use VSIX in downstream jobs

--- .github/workflows/scripts/setup-linux.sh
+++ .github/workflows/scripts/setup-linux.sh
@@ -18,6 +18,13 @@ export NODE_PATH=/usr/local/nvm/versions/node/v20.18.2/bin
 export NVM_DIR=/usr/local/nvm
 
 apt-get update && apt-get install -y rsync curl gpg libasound2 libgbm1 libgtk-3-0 libnss3 xvfb build-essential
+
+if [ ! -z "$VSCODE_SWIFT_VSIX_URL" ]; then
+    export VSCODE_SWIFT_VSIX="$PWD/vscode-swift.vsix"
+    echo "Downloading $VSCODE_SWIFT_VSIX_URL to $VSCODE_SWIFT_VSIX"
+    curl -o $VSCODE_SWIFT_VSIX "$VSCODE_SWIFT_VSIX_URL" 
+fi
+
 mkdir -p $NVM_DIR
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 /bin/bash -c "source $NVM_DIR/nvm.sh && nvm install $NODE_VERSION"
