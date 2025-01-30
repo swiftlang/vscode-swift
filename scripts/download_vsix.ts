84b62364e83053a711109d45203387763b44e642
Adam Ward <award999@apple.com>
2025-01-30 10:15:50 -0500

    Make work for Windows

---
+++ scripts/download_vsix.ts
@@ -0,0 +1,61 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2025 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+/* eslint-disable no-console */
+
+import decompress from "decompress";
+import { createWriteStream } from "node:fs";
+import { rename, unlink } from "node:fs/promises";
+import { pipeline } from "node:stream/promises";
+import { Octokit } from "octokit";
+
+const artifact_id = process.env["VSCODE_SWIFT_VSIX_ID"];
+if (!artifact_id) {
+    console.error("No VSCODE_SWIFT_VSIX_ID provided");
+    process.exit(1);
+}
+const token = process.env["GITHUB_TOKEN"];
+if (!token) {
+    console.error("No GITHUB_TOKEN provided");
+    process.exit(1);
+}
+const repository = process.env["GITHUB_REPOSITORY"] || "swiftlang/vscode-swift";
+
+(async function () {
+    const owner = repository.split("/")[0];
+    const repo = repository.split("/")[1];
+
+    const octokit = new Octokit({
+        auth: token,
+    });
+
+    const { data } = await octokit.request(
+        `GET /repos/${repository}/actions/artifacts/${artifact_id}/zip`,
+        {
+            request: {
+                parseSuccessResponseBody: false,
+            },
+            owner,
+            repo,
+            artifact_id,
+            archive_format: "zip",
+            headers: {
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+        }
+    );
+    await pipeline(data, createWriteStream("artifacts.zip", data));
+    const files = await decompress("artifacts.zip", process.cwd());
+    await rename(files[0].path, "vscode-swift.vsix");
+    await unlink("artifacts.zip");
+})();
