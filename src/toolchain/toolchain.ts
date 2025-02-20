7c1fa056c6d9747645396db221c7e1c152c9fb32
Paul LeMarquand <plemarquand@apple.com>
2025-02-20 08:58:36 -0500

    Add PrivateFrameworks as additional Swift Testing framework path (#1396)
    This is essentially the same change made in:
    https://github.com/swiftlang/swift-package-manager/pull/8199
    
    VS Code would be able to leverage it for regular runs with `swift test`
    but because when debugging we launch the xctest executable directly to
    run tests, we must include the same framework search paths as SPM.

--- src/toolchain/toolchain.ts
+++ src/toolchain/toolchain.ts
@@ -462,7 +462,9 @@ export class SwiftToolchain {
         if (!base) {
             return undefined;
         }
-        return path.join(base, "Library/Frameworks");
+        const frameworks = path.join(base, "Library/Frameworks");
+        const privateFrameworks = path.join(base, "Library/PrivateFrameworks");
+        return `${frameworks}:${privateFrameworks}`;
     }
 
     get diagnostics(): string {
