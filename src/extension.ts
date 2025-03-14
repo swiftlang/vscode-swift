4121e2e98264b100e12cf8c11796ee644dfdccac
Rishi <rbenegal@apple.com>
2025-03-14 13:46:04 -0400

    Set swift.swiftSDK for target platforms (#1390)
    * Set swift.swiftSDK for target platforms
    
    SourceKit-LSP now provides code editing support for non-macOS Darwin
    platforms starting Swift 6.1 using the --swift-sdk flag. Set this flag
    to the appropriate target triple when using the "Select Target Platform"
    feature on macOS.
    
    Issue: #1335
    
    * Restrict Switch Platform to macOS and Swift 6.1
    
    Currently this command only supports Darwin SDKs on macOS, and code
    editing support from Swift 6.1 and above. Only show the command for
    these cases.
    
    Also add a unit test for the command.
    
    Issue: #1335

--- src/extension.ts
+++ src/extension.ts
@@ -76,12 +76,16 @@ export async function activate(context: vscode.ExtensionContext): Promise<Api> {
                 contextKeys.createNewProjectAvailable = toolchain.swiftVersion.isGreaterThanOrEqual(
                     new Version(5, 8, 0)
                 );
+                contextKeys.switchPlatformAvailable = toolchain.swiftVersion.isGreaterThanOrEqual(
+                    new Version(6, 1, 0)
+                );
                 return toolchain;
             })
             .catch(error => {
                 outputChannel.log("Failed to discover Swift toolchain");
                 outputChannel.log(error);
                 contextKeys.createNewProjectAvailable = false;
+                contextKeys.switchPlatformAvailable = false;
                 return undefined;
             });
 
@@ -102,11 +106,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<Api> {
                     event.affectsConfiguration("swift.SDK") ||
                     event.affectsConfiguration("swift.swiftSDK")
                 ) {
-                    // FIXME: There is a bug stopping us from restarting SourceKit-LSP directly.
-                    // As long as it's fixed we won't need to reload on newer versions.
-                    showReloadExtensionNotification(
-                        "Changing the Swift SDK path requires the project be reloaded."
-                    );
+                    vscode.commands.executeCommand("swift.restartLSPServer");
                 } else if (event.affectsConfiguration("swift.swiftEnvironmentVariables")) {
                     showReloadExtensionNotification(
                         "Changing environment variables requires the project be reloaded."
