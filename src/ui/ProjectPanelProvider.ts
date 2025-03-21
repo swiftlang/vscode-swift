43933d118127cbfdf1ecb4601325f111063b07c3
Paul LeMarquand <plemarquand@apple.com>
2025-03-20 20:13:52 -0400

    Show all task types in project panel
    If users want to have non-Swift task types in their package they should
    be able to.

--- src/ui/ProjectPanelProvider.ts
+++ src/ui/ProjectPanelProvider.ts
@@ -523,7 +523,6 @@ export class ProjectPanelProvider implements vscode.TreeDataProvider<TreeNode> {
                 // Plugin tasks are shown under the Commands header
                 .filter(
                     task =>
-                        task.definition.type === "swift" &&
                         task.definition.cwd === folderContext.folder.fsPath &&
                         task.source !== "swift-plugin"
                 )
