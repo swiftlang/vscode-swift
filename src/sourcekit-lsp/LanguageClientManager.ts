fc7fbe1260b8c71212a569d30cbd068059aada12
Paul LeMarquand <plemarquand@apple.com>
2025-03-28 09:16:34 -0400

    workspace/_synchronize has been promoted to non-experimental

--- src/sourcekit-lsp/LanguageClientManager.ts
+++ src/sourcekit-lsp/LanguageClientManager.ts
@@ -679,13 +679,6 @@ export class LanguageClientManager implements vscode.Disposable {
             };
         }
 
-        if (this.swiftVersion.isGreaterThanOrEqual(new Version(6, 2, 0))) {
-            options = {
-                ...options,
-                experimentalFeatures: ["synchronize-request"],
-            };
-        }
-
         if (configuration.swiftSDK !== "") {
             options = {
                 ...options,
