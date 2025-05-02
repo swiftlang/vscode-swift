f75f70310b5c70dbf3baf3ef2e2a87f0bed81db4
Adam Ward <award999@apple.com>
2025-05-02 09:53:43 -0400

    Mark vscode-swift directory as safe

--- scripts/test.sh
+++ scripts/test.sh
@@ -31,7 +31,6 @@ cd /tmp/code
 npm ci
 npm run lint
 npm run format
-npm run package
 npm run soundness -- --force-run
 
 xvfb-run -a npm run coverage 2>&1 | grep -Ev "Failed to connect to the bus|GPU stall due to ReadPixels"
