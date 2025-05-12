1275cecf1027af660a5b0518e08d2023372e76f2
award999 <award999@apple.com>
2025-05-12 10:16:07 -0400

    Fix reseting of settings (#1546)
    Fix logic to reset these when changed, also consider if default value
    when reseting. This causes backgroundCompilation on in test project
    to get removed since that is the default

--- test/integration-tests/utilities/testutilities.ts
+++ test/integration-tests/utilities/testutilities.ts
@@ -140,6 +140,7 @@ const extensionBootstrapper = (() => {
                 if (autoTeardown) {
                     await autoTeardown();
                 }
+                await waitForNoRunningTasks();
             } catch (error) {
                 if (workspaceContext) {
                     printLogs(workspaceContext.outputChannel, "Error during test/suite teardown");
@@ -340,10 +341,13 @@ export async function updateSettings(settings: SettingsMap): Promise<() => Promi
         for (const setting of Object.keys(settings)) {
             const { section, name } = decomposeSettingName(setting);
             const config = vscode.workspace.getConfiguration(section, { languageId: "swift" });
-            savedOriginalSettings[setting] = config.get(name);
+            const inspectedSetting = vscode.workspace
+                .getConfiguration(section, { languageId: "swift" })
+                .inspect(name);
+            savedOriginalSettings[setting] = inspectedSetting?.workspaceValue;
             await config.update(
                 name,
-                settings[setting] === "" ? undefined : settings[setting],
+                !settings[setting] ? undefined : settings[setting],
                 vscode.ConfigurationTarget.Workspace
             );
         }
