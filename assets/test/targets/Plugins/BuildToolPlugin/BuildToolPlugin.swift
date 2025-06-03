e36f8e8b493bb0aa87e7aa12bc0036b6ad32a3ed
Paul LeMarquand <plemarquand@apple.com>
2025-06-03 13:35:44 -0400

    Fix build tool plugin on Windows

--- assets/test/targets/Plugins/BuildToolPlugin/BuildToolPlugin.swift
+++ assets/test/targets/Plugins/BuildToolPlugin/BuildToolPlugin.swift
@@ -1,4 +1,5 @@
 import PackagePlugin
+import Foundation
 
 @main
 struct SimpleBuildToolPlugin: BuildToolPlugin {
@@ -9,28 +10,35 @@ struct SimpleBuildToolPlugin: BuildToolPlugin {
 
         // Construct a build command for each source file with a particular suffix.
         return sourceFiles.map(\.path).compactMap {
-            createBuildCommand(for: $0, in: context.pluginWorkDirectory, with: generatorTool.path)
+            createBuildCommand(
+                for: $0,
+                in: context.pluginWorkDirectory,
+                with: generatorTool.path
+            )
         }
     }
 
     /// Calls a build tool that transforms JSON files into Swift files.
     func createBuildCommand(for inputPath: Path, in outputDirectoryPath: Path, with generatorToolPath: Path) -> Command? {
+        let inputURL = URL(fileURLWithPath: inputPath.string)
+        let outputDirectoryURL = URL(fileURLWithPath: outputDirectoryPath.string)
+
         // Skip any file that doesn't have the extension we're looking for (replace this with the actual one).
-        guard inputPath.extension == "json" else { return .none }
+        guard inputURL.pathExtension == "json" else { return .none }
 
         // Produces .swift files in the same directory structure as the input JSON files appear in the target.
-        let components = inputPath.string.split(separator: "LibraryTarget", omittingEmptySubsequences: false).map(String.init)
-        let inputName = inputPath.lastComponent
-        let outputDir = outputDirectoryPath.appending(components[1]).removingLastComponent()
-        let outputName = inputPath.stem + ".swift"
-        let outputPath = outputDir.appending(outputName)
+        let components = inputURL.absoluteString.split(separator: "LibraryTarget", omittingEmptySubsequences: false).map(String.init)
+        let inputName = inputURL.lastPathComponent
+        let outputDir = outputDirectoryURL.appending(path: components[1]).deletingLastPathComponent()
+        let outputName = inputURL.deletingPathExtension().lastPathComponent + ".swift"
+        let outputURL = outputDir.appending(path: outputName)
 
         return .buildCommand(
             displayName: "Generating \(outputName) from \(inputName)",
             executable: generatorToolPath,
-            arguments: ["\(inputPath)", "\(outputPath)"],
+            arguments: ["\(inputPath)", "\(outputURL.path)"],
             inputFiles: [inputPath],
-            outputFiles: [outputPath]
+            outputFiles: [Path(outputURL.path)],
         )
     }
 }
