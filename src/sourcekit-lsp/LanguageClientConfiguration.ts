0bba771a2a5cb8d16ed659a89db4ab13f319b352
Alex Hoppen <ahoppen@apple.com>
2025-07-14 17:05:45 +0200

    Accept `Location` for `workspace/peekDocument`
    This allows the peek window to scroll to a position within the peeked document.

--- src/sourcekit-lsp/LanguageClientConfiguration.ts
+++ src/sourcekit-lsp/LanguageClientConfiguration.ts
@@ -45,6 +45,7 @@ function initializationOptions(swiftVersion: Version): any {
         options = {
             "workspace/peekDocuments": {
                 supported: true, // workaround for client capability to handle `PeekDocumentsRequest`
+                peekLocation: true, // allow SourceKit-LSP to send `Location` instead of `DocumentUri` for the locations to peek.
             },
             "workspace/getReferenceDocument": {
                 supported: true, // the client can handle URIs with scheme `sourcekit-lsp:`
