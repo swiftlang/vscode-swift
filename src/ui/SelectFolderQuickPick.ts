9f874ecdb9721dc261a488fb1d820b31a6eff894
award999 <award999@apple.com>
2025-07-21 10:19:56 -0400

    Add a new `Generate SourceKit-LSP Configuration` command (#1726)
    * Add a new `Generate SourceKit-LSP Configuration` command
    
    * Add CHANGELOG entry
    
    * Add documentation
    
    * Fix failing test
    
    * Fix review comment

---
+++ src/ui/SelectFolderQuickPick.ts
@@ -0,0 +1,64 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2025 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import { WorkspaceContext } from "../WorkspaceContext";
+import * as vscode from "vscode";
+import { FolderContext } from "../FolderContext";
+
+type SelectFolderQuickPick = AllQuickPickItem | FolderQuickPickItem;
+
+interface AllQuickPickItem extends vscode.QuickPickItem {
+    type: "all";
+}
+
+interface FolderQuickPickItem extends vscode.QuickPickItem {
+    type: "folder";
+    folder: FolderContext;
+}
+
+/**
+ * Select a folder from the workspace context
+ * @param ctx
+ * @param labels Map "type" to the display label
+ * @returns The selected folder or undefined if there was no selection
+ */
+export async function selectFolder(
+    ctx: WorkspaceContext,
+    placeHolder: string,
+    labels: Record<string, string> = {}
+): Promise<FolderContext[]> {
+    const quickPickItems: SelectFolderQuickPick[] = ctx.folders.map(folder => ({
+        type: "folder",
+        folder,
+        label: folder.name,
+        detail: folder.workspaceFolder.uri.fsPath,
+    }));
+    quickPickItems.push({ type: "all", label: labels["all"] || "Generate For All Folders" });
+    const selection = await vscode.window.showQuickPick(quickPickItems, {
+        matchOnDetail: true,
+        placeHolder,
+    });
+
+    const folders: FolderContext[] = [];
+    if (!selection) {
+        return folders;
+    }
+
+    if (selection.type === "all") {
+        folders.push(...ctx.folders);
+    } else {
+        folders.push(selection.folder);
+    }
+    return folders;
+}
---
+++ test/integration-tests/commands/generateSourcekitConfiguration.test.ts
@@ -0,0 +1,115 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2025 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import * as vscode from "vscode";
+import { expect } from "chai";
+import { FolderContext } from "../../../src/FolderContext";
+import { WorkspaceContext } from "../../../src/WorkspaceContext";
+import { Commands } from "../../../src/commands";
+import {
+    activateExtensionForSuite,
+    folderInRootWorkspace,
+    updateSettings,
+} from "../utilities/testutilities";
+import { closeAllEditors } from "../../utilities/commands";
+import {
+    sourcekitConfigFilePath,
+    sourcekitFolderPath,
+} from "../../../src/commands/generateSourcekitConfiguration";
+import { Version } from "../../../src/utilities/version";
+
+suite("Generate SourceKit-LSP configuration Command", function () {
+    let folderContext: FolderContext;
+    let workspaceContext: WorkspaceContext;
+    let resetSettings: (() => Promise<void>) | undefined;
+
+    activateExtensionForSuite({
+        async setup(ctx) {
+            workspaceContext = ctx;
+            folderContext = await folderInRootWorkspace("defaultPackage", workspaceContext);
+            await workspaceContext.focusFolder(folderContext);
+        },
+    });
+
+    teardown(async () => {
+        if (resetSettings) {
+            await resetSettings();
+        }
+        await vscode.workspace.fs.delete(vscode.Uri.file(sourcekitFolderPath(folderContext)), {
+            recursive: true,
+        });
+    });
+
+    suiteTeardown(async () => {
+        await closeAllEditors();
+    });
+
+    test("Calculates branch based on toolchain", async () => {
+        const result = await vscode.commands.executeCommand(Commands.GENERATE_SOURCEKIT_CONFIG);
+        expect(result).to.be.true;
+        const contents = Buffer.from(
+            await vscode.workspace.fs.readFile(
+                vscode.Uri.file(sourcekitConfigFilePath(folderContext))
+            )
+        ).toString("utf-8");
+        const config = JSON.parse(contents);
+        const version = folderContext.swiftVersion;
+        let branch: string;
+        if (folderContext.swiftVersion.isGreaterThanOrEqual(new Version(6, 1, 0))) {
+            branch = version.dev ? "main" : `release/${version.major}.${version.minor}`;
+        } else {
+            branch = "main";
+        }
+        expect(config).to.have.property(
+            "$schema",
+            `https://raw.githubusercontent.com/swiftlang/sourcekit-lsp/refs/heads/${branch}/config.schema.json`
+        );
+    });
+
+    test("Uses hardcoded path", async () => {
+        resetSettings = await updateSettings({
+            "swift.sourcekit-lsp.configurationBranch": "release/6.1",
+        });
+        const result = await vscode.commands.executeCommand(Commands.GENERATE_SOURCEKIT_CONFIG);
+        expect(result).to.be.true;
+        const contents = Buffer.from(
+            await vscode.workspace.fs.readFile(
+                vscode.Uri.file(sourcekitConfigFilePath(folderContext))
+            )
+        ).toString("utf-8");
+        const config = JSON.parse(contents);
+        expect(config).to.have.property(
+            "$schema",
+            `https://raw.githubusercontent.com/swiftlang/sourcekit-lsp/refs/heads/release/6.1/config.schema.json`
+        );
+    });
+
+    test('Fallsback to "main" when path does not exist', async () => {
+        resetSettings = await updateSettings({
+            "swift.sourcekit-lsp.configurationBranch": "totally-invalid-branch",
+        });
+        const result = await vscode.commands.executeCommand(Commands.GENERATE_SOURCEKIT_CONFIG);
+        expect(result).to.be.true;
+        const contents = Buffer.from(
+            await vscode.workspace.fs.readFile(
+                vscode.Uri.file(sourcekitConfigFilePath(folderContext))
+            )
+        ).toString("utf-8");
+        const config = JSON.parse(contents);
+        expect(config).to.have.property(
+            "$schema",
+            `https://raw.githubusercontent.com/swiftlang/sourcekit-lsp/refs/heads/main/config.schema.json`
+        );
+    });
+});
