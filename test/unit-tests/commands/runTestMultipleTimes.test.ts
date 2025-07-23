6cdc4e00f323375e29ff4a9fbd623c9e45882c99
Matthew Bastien <matthew_bastien@apple.com>
2025-07-23 11:16:06 -0400

    add missing copyright header

--- test/unit-tests/commands/runTestMultipleTimes.test.ts
+++ test/unit-tests/commands/runTestMultipleTimes.test.ts
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2025 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import * as vscode from "vscode";
 import { expect } from "chai";
 import { extractTestItemsAndCount } from "../../../src/commands/testMultipleTimes";
