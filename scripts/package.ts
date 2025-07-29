968a522fed6d85d72bb46b7d92cfa6840c4cd165
Adam Ward <award999@apple.com>
2025-07-29 13:01:19 -0400

    Bump extension version

--- scripts/package.ts
+++ scripts/package.ts
@@ -13,18 +13,12 @@
 //===----------------------------------------------------------------------===//
 /* eslint-disable no-console */
 
-import {
-    exec,
-    getExtensionVersion,
-    getRootDirectory,
-    main,
-    updateChangelog,
-} from "./lib/utilities";
+import { getExtensionVersion, main, packageExtension } from "./lib/utilities";
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 main(async () => {
-    const rootDirectory = getRootDirectory();
     const version = await getExtensionVersion();
+    // Leave the "prerelease" tag out
     const versionString = `${version.major}.${version.minor}.${version.patch}`;
 
     if (process.platform === "win32") {
@@ -32,15 +26,5 @@ main(async () => {
         return process.exit(0);
     }
 
-    // Update version in CHANGELOG
-    await updateChangelog(versionString);
-
-    // Use VSCE to package the extension
-    // Note: There are no sendgrid secrets in the extension. `--allow-package-secrets` works around a false positive
-    // where the symbol `SG.MessageTransports.is` can appear in the dist.js if we're unlucky enough
-    // to have `SG` as the minified name of a namespace. Here is the rule we sometimes mistakenly match:
-    // https://github.com/secretlint/secretlint/blob/5706ac4942f098b845570541903472641d4ae914/packages/%40secretlint/secretlint-rule-sendgrid/src/index.ts#L35
-    await exec("npx", ["vsce", "package", "--allow-package-secrets", "sendgrid"], {
-        cwd: rootDirectory,
-    });
+    await packageExtension(versionString);
 });
