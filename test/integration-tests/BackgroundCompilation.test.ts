3139ec12d6936af36484a8eebff70710d0088b9a
Matthew Bastien <matthew_bastien@apple.com>
2025-09-03 16:48:15 -0400

    dispose of subscriptions in Background Compilation tests

--- test/integration-tests/BackgroundCompilation.test.ts
+++ test/integration-tests/BackgroundCompilation.test.ts
@@ -19,13 +19,16 @@ import { WorkspaceContext } from "@src/WorkspaceContext";
 import { testAssetUri } from "../fixtures";
 import { tag } from "../tags";
 import { closeAllEditors } from "../utilities/commands";
+import { waitForNoRunningTasks } from "../utilities/tasks";
 import { activateExtensionForTest, updateSettings } from "./utilities/testutilities";
 
 tag("large").suite("BackgroundCompilation Test Suite", () => {
+    let subscriptions: vscode.Disposable[];
     let workspaceContext: WorkspaceContext;
 
     activateExtensionForTest({
         async setup(ctx) {
+            subscriptions = [];
             workspaceContext = ctx;
             assert.notEqual(workspaceContext.folders.length, 0);
             return await updateSettings({
@@ -35,21 +38,20 @@ tag("large").suite("BackgroundCompilation Test Suite", () => {
     });
 
     suiteTeardown(async () => {
+        subscriptions.forEach(s => s.dispose());
         await closeAllEditors();
     });
 
     test("build all on save", async () => {
-        const taskPromise = new Promise<void>(res => {
-            vscode.tasks.onDidStartTask(e => {
-                const task = e.execution.task;
-                if (task.name.includes("Build All")) {
-                    vscode.tasks.onDidEndTask(e => {
-                        if (e.execution.task === task) {
-                            res();
-                        }
-                    });
-                }
-            });
+        const taskStartPromise = new Promise<void>(resolve => {
+            subscriptions.push(
+                vscode.tasks.onDidStartTask(e => {
+                    const task = e.execution.task;
+                    if (task.name.includes("Build All")) {
+                        resolve();
+                    }
+                })
+            );
         });
 
         const uri = testAssetUri("defaultPackage/Sources/PackageExe/main.swift");
@@ -57,6 +59,7 @@ tag("large").suite("BackgroundCompilation Test Suite", () => {
         await vscode.window.showTextDocument(doc);
         await vscode.workspace.save(uri);
 
-        await taskPromise;
+        await taskStartPromise;
+        await waitForNoRunningTasks();
     });
 });
