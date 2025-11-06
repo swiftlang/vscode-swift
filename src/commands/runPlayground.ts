3fa2118a31e8ee20325f415cf936855f37650279
Adam Ward <award999@apple.com>
2025-11-06 08:04:56 -0500

    Add copyright header

--- src/commands/runPlayground.ts
+++ src/commands/runPlayground.ts
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2025 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 import * as vscode from "vscode";
 import { Location, Range } from "vscode-languageclient";
 
