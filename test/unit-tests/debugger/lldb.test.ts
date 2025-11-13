c2116fc2a361f4f16aaad31ed205636c131a042e
Matthew Bastien <matthew_bastien@apple.com>
2025-11-13 17:24:37 -0500

    detect xcrun shims

--- test/unit-tests/debugger/lldb.test.ts
+++ test/unit-tests/debugger/lldb.test.ts
@@ -12,7 +12,6 @@
 //
 //===----------------------------------------------------------------------===//
 import { expect } from "chai";
-import * as fs from "fs/promises";
 import * as sinon from "sinon";
 
 import * as lldb from "@src/debugger/lldb";
@@ -20,7 +19,6 @@ import { SwiftToolchain } from "@src/toolchain/toolchain";
 import * as util from "@src/utilities/utilities";
 
 import {
-    MockedFunction,
     MockedObject,
     instance,
     mockFn,
@@ -30,18 +28,20 @@ import {
 } from "../../MockUtils";
 
 suite("debugger.lldb Tests", () => {
-    suite("getLLDBLibPath Tests", () => {
+    suite("getLLDBLibPath()", () => {
         let mockToolchain: MockedObject<SwiftToolchain>;
-        let mockFindLibLLDB: MockedFunction<(typeof lldb)["findLibLLDB"]>;
         const mockedPlatform = mockGlobalValue(process, "platform");
         const mockUtil = mockGlobalModule(util);
 
         setup(() => {
-            mockFindLibLLDB = sinon.stub();
             mockToolchain = mockObject<SwiftToolchain>({
                 getLLDB: mockFn(),
-                swiftFolderPath: "",
+                toolchainPath: "",
             });
+        });
+
+        teardown(() => {
+            sinon.restore();
         });
 
         test("should return failure if toolchain.getLLDB() throws an error", async () => {
@@ -58,79 +58,6 @@ suite("debugger.lldb Tests", () => {
             // specific behaviour: return success and failure both undefined
             expect(result.failure).to.equal(undefined);
             expect(result.success).to.equal(undefined);
-        });
-
-        test("should return failure if findLibLLDB returns falsy values", async () => {
-            mockToolchain.getLLDB.resolves("/path/to/lldb");
-            mockUtil.execFile.resolves({ stdout: "", stderr: "" });
-            mockFindLibLLDB.onFirstCall().resolves(undefined);
-
-            let result = await lldb.getLLDBLibPath(instance(mockToolchain));
-            expect(result.failure).to.not.equal(undefined);
-
-            mockFindLibLLDB.onSecondCall().resolves("");
-
-            result = await lldb.getLLDBLibPath(instance(mockToolchain));
-            expect(result.failure).to.not.equal(undefined);
-        });
-        // NB(separate itest): contract test with toolchains of various platforms
-    });
-
-    suite("findLibLLDB Tests", () => {
-        const fsMock = mockGlobalModule(fs);
-
-        test("should return undefined if no file matches the pattern", async () => {
-            fsMock.readdir.resolves(["file1", "file2"] as any);
-            fsMock.stat.resolves({ isFile: () => false } as any);
-
-            const result = await lldb.findLibLLDB("/path/hint");
-
-            expect(result).to.be.undefined;
-        });
-
-        test("should return path if file exists", async () => {
-            fsMock.stat.resolves({ isFile: () => true } as any);
-
-            const result = await lldb.findLibLLDB("/path/hint");
-
-            expect(result).to.equal("/path/hint");
-        });
-        // NB(separate itest): contract test with toolchains of various platforms
-    });
-
-    suite("findFileByPattern Tests", () => {
-        const fsMock = mockGlobalModule(fs);
-
-        test("should return null if no file matches the pattern", async () => {
-            fsMock.readdir.resolves(["file1", "file2"] as any);
-
-            const result = await lldb.findFileByPattern("/some/path", /pattern/);
-
-            expect(result).to.be.null;
-        });
-
-        test("should return the first match if one file matches the pattern", async () => {
-            fsMock.readdir.resolves(["match1", "nomatch"] as any);
-
-            const result = await lldb.findFileByPattern("/some/path", /match1/);
-
-            expect(result).to.equal("match1");
-        });
-
-        test("should return the first match if multiple files match the pattern", async () => {
-            fsMock.readdir.resolves(["match1", "match2"] as any);
-
-            const result = await lldb.findFileByPattern("/some/path", /match/);
-
-            expect(result).to.equal("match1");
-        });
-
-        test("should return null if directory reading fails", async () => {
-            fsMock.readdir.rejects(new Error("Some error"));
-
-            const result = await lldb.findFileByPattern("/some/path", /pattern/);
-
-            expect(result).to.be.null;
         });
     });
 });
