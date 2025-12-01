83885b19c8a5eb255c6516a598e574d27b44945e
award999 <award999@apple.com>
2025-12-01 07:51:42 -0500

    Support "swift.play" CodeLens (#1924)
    * Support "swift.play" CodeLens
    
    - Advertise to the LSP we support "swift.play"
    - Add a "swift.play" command, hiding it from the command palette
    - Fix order of env variables passed to task so variables like
      DYLD_LIBRARY_PATH are not overwritten accidentally
    
    Issue: #1782
    
    * Add copyright header
    
    * Add tests
    
    * Restructure to make more testable
    
    * Fix review comment

---
+++ src/commands/runPlayground.ts
@@ -0,0 +1,58 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2025 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+import * as vscode from "vscode";
+import { Location, Range } from "vscode-languageclient";
+
+import { FolderContext } from "../FolderContext";
+import { createSwiftTask } from "../tasks/SwiftTaskProvider";
+import { TaskManager } from "../tasks/TaskManager";
+import { packageName } from "../utilities/tasks";
+
+export interface PlaygroundItem {
+    id: string;
+    label?: string;
+}
+
+export interface DocumentPlaygroundItem extends PlaygroundItem {
+    range: Range;
+}
+
+export interface WorkspacePlaygroundItem extends PlaygroundItem {
+    location: Location;
+}
+
+/**
+ * Executes a {@link vscode.Task task} to run swift playground.
+ */
+export async function runPlayground(
+    folderContext: FolderContext,
+    tasks: TaskManager,
+    item: PlaygroundItem
+) {
+    const id = item.label ?? item.id;
+    const task = createSwiftTask(
+        ["play", id],
+        `Play "${id}"`,
+        {
+            cwd: folderContext.folder,
+            scope: folderContext.workspaceFolder,
+            packageName: packageName(folderContext),
+            presentationOptions: { reveal: vscode.TaskRevealKind.Always },
+        },
+        folderContext.toolchain
+    );
+
+    await tasks.executeTaskAndWait(task);
+    return true;
+}
