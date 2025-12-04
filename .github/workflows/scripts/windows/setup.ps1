6fe981eb5af3abc4fa7bfed2450d1d9f8a8b7d69
Matthew Bastien <matthew_bastien@apple.com>
2025-12-03 19:13:29 -0500

    remove unnecessary environment update

--- .github/workflows/scripts/windows/setup.ps1
+++ .github/workflows/scripts/windows/setup.ps1
@@ -10,8 +10,3 @@ if ($Process.ExitCode -eq 0) {
     Write-Host  ('FAILED ({0})' -f $Process.ExitCode)
     exit 1
 }
-
-Get-Content $env:GITHUB_ENV | foreach {
-    $name, $value = $_.split('=')
-    Set-Content env:\$name $value
-}
