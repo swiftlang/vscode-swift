4d49434d5437521494f28e9761bb956ee88452c9
Matthew Bastien <matthew_bastien@apple.com>
2025-12-10 14:58:44 -0500

    don't use Start-Process for npm/npx on Windows

--- .github/workflows/scripts/windows/setup.ps1
+++ .github/workflows/scripts/windows/setup.ps1
@@ -3,10 +3,7 @@
 
 # Download the VSIX archived upstream
 npm ci --ignore-scripts
-$Process = Start-Process npx "tsx scripts/download_vsix.ts" -Wait -PassThru -NoNewWindow
-if ($Process.ExitCode -eq 0) {
-    Write-Host 'SUCCESS'
-} else {
-    Write-Host  ('FAILED ({0})' -f $Process.ExitCode)
-    exit 1
+npx tsx scripts/download_vsix.ts
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode
 }
