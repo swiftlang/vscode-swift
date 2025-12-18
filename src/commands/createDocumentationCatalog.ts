907fdfb2e9fae44dd88e088cffd8ca5e00277044
Aman Maurya <amanthatdoescares@gmail.com>
2025-12-18 12:13:27 +0530

    Update src/commands/createDocumentationCatalog.ts
    Co-authored-by: Matthew Bastien <matthew_bastien@apple.com>

--- src/commands/createDocumentationCatalog.ts
+++ src/commands/createDocumentationCatalog.ts
@@ -2,7 +2,7 @@
 //
 // This source file is part of the VS Code Swift open source project
 //
-// Copyright (c) 2021-2024 the VS Code Swift project authors
+// Copyright (c) 2025 the VS Code Swift project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
