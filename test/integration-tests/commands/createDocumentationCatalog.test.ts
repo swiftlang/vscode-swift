f991a9cf877008ef24f1df1c5f1bfdc5cdf90fbd
Aman <amanthatdoescares@gmail.com>
2025-12-22 14:23:44 +0530

    remove nested test, mark suite as small, added right header

--- test/integration-tests/commands/createDocumentationCatalog.test.ts
+++ test/integration-tests/commands/createDocumentationCatalog.test.ts
@@ -5,6 +5,9 @@
 // Copyright (c) 2025 the VS Code Swift project authors
 // Licensed under Apache License v2.0
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
@@ -20,7 +23,7 @@ import { WorkspaceContext } from "@src/WorkspaceContext";
 import { tag } from "../../tags";
 import { activateExtensionForSuite, folderInRootWorkspace } from "../utilities/testutilities";
 
-tag("large").suite("Create Documentation Catalog Command", function () {
+tag("small").suite("Create Documentation Catalog Command", function () {
     let folderContext: FolderContext;
     let workspaceContext: WorkspaceContext;
 
@@ -33,31 +36,30 @@ tag("large").suite("Create Documentation Catalog Command", function () {
     });
 
     test("creates a DocC catalog for a SwiftPM target", async () => {
-        test("creates a DocC catalog for a SwiftPM target", async () => {
-            const quickPickStub = sinon.stub(vscode.window, "showQuickPick");
-            const inputBoxStub = sinon.stub(vscode.window, "showInputBox");
+        const quickPickStub = sinon.stub(vscode.window, "showQuickPick");
+        const inputBoxStub = sinon.stub(vscode.window, "showInputBox");
 
-            try {
-                inputBoxStub.resolves("MyModule");
-                quickPickStub.callsFake(async itemsOrPromise => {
-                    const items = await Promise.resolve(itemsOrPromise);
-                    return items.find(item => item.label.startsWith("Target:"));
-                });
+        try {
+            inputBoxStub.resolves("MyModule");
+            quickPickStub.callsFake(async itemsOrPromise => {
+                const items = await Promise.resolve(itemsOrPromise);
+                return items.find(item => item.label.startsWith("Target:"));
+            });
 
-                await vscode.commands.executeCommand("swift.createDocumentationCatalog");
-                const basePath = folderContext.folder.fsPath;
-                const doccDir = path.join(basePath, "MyModule.docc");
-                const markdownFile = path.join(doccDir, "MyModule.md");
+            await vscode.commands.executeCommand("swift.createDocumentationCatalog");
 
-                expect(await fs.stat(doccDir)).to.exist;
-                expect(await fs.stat(markdownFile)).to.exist;
+            const basePath = folderContext.folder.fsPath;
+            const doccDir = path.join(basePath, "MyModule.docc");
+            const markdownFile = path.join(doccDir, "MyModule.md");
 
-                const contents = await fs.readFile(markdownFile, "utf8");
-                expect(contents).to.contain("# MyModule");
-            } finally {
-                quickPickStub.restore();
-                inputBoxStub.restore();
-            }
-        });
+            expect(await fs.stat(doccDir)).to.exist;
+            expect(await fs.stat(markdownFile)).to.exist;
+
+            const contents = await fs.readFile(markdownFile, "utf8");
+            expect(contents).to.contain("# MyModule");
+        } finally {
+            quickPickStub.restore();
+            inputBoxStub.restore();
+        }
     });
 });
