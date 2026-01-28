4c374da186bdedb0a73370e727fff50aac015fc6
Paul LeMarquand <plemarquand@apple.com>
2026-01-28 09:44:39 -0500

    Mark swift-testing test runs as started immediately (#2039)
    * Mark swift-testing test runs as started immediately
    
    Previously we were waiting for all parameterized test items to be
    emitted on the JSON event stream before starting the `vscode.TestRun`.
    
    This limitation was in place to ensure we had all the test items in test
    request sent to VSCode, as we couldn't add test items after a run had
    started.
    
    That requirement seems to have been relaxed, or we were just not
    refreshing the test explorer correctly after dynamically adding items,
    but regardless it looks like we can dynamically add items now.
    
    This means we can correctly mark a test run as "started" immediately
    when the user starts compiling a test like we do with XCTest, making the
    two testing libraries the same in this regard. It also simplifies the
    code, allowing us to accept and inject new parameterized test items at
    any time during a run.
    
    * Update changelog

--- src/TestExplorer/TestRunner.ts
+++ src/TestExplorer/TestRunner.ts
@@ -83,7 +83,6 @@ export interface TestRunState {
 
 export class TestRunProxy {
     private testRun?: vscode.TestRun;
-    private addedTestItems: { testClass: TestClass; parentIndex: number }[] = [];
     private runStarted: boolean = false;
     private queuedOutput: string[] = [];
     private _testItems: vscode.TestItem[];
@@ -146,14 +145,23 @@ export class TestRunProxy {
         this.resetTags(this.controller);
         this.runStarted = true;
 
-        // When a test run starts we need to do several things:
-        // - Create new TestItems for each paramterized test that was added
-        //   and attach them to their parent TestItem.
-        // - Create a new test run from the TestRunArguments + newly created TestItems.
-        // - Mark all of these test items as enqueued on the test run.
+        this.testRun = this.controller.createTestRun(this.testRunRequest);
+        this.token.add(this.testRun.token);
 
-        const addedTestItems = this.addedTestItems
-            .map(({ testClass, parentIndex }) => {
+        // Forward any output captured before the testRun was created.
+        for (const outputLine of this.queuedOutput) {
+            this.performAppendOutput(this.testRun, outputLine);
+        }
+        this.queuedOutput = [];
+
+        for (const test of this.testItems) {
+            this.enqueued(test);
+        }
+    };
+
+    public addParameterizedTestCases = (testClasses: TestClass[], parentIndex: number) => {
+        const addedTestItems = testClasses
+            .map(testClass => {
                 const parent = this.args.testItems[parentIndex];
                 // clear out the children before we add the new ones.
                 parent.children.replace([]);
@@ -189,34 +197,17 @@ export class TestRunProxy {
 
                 return added;
             });
-
-        this.testRun = this.controller.createTestRun(this.testRunRequest);
-        this.token.add(this.testRun.token);
-
-        const existingTestItemCount = this.testItems.length;
         this._testItems = [...this.testItems, ...addedTestItems];
 
-        if (this._testItems.length !== existingTestItemCount) {
-            // Recreate a test item finder with the added test items
-            this.testItemFinder =
-                process.platform === "darwin"
-                    ? new DarwinTestItemFinder(this.testItems)
-                    : new NonDarwinTestItemFinder(this.testItems, this.folderContext);
-        }
-
-        // Forward any output captured before the testRun was created.
-        for (const outputLine of this.queuedOutput) {
-            this.performAppendOutput(this.testRun, outputLine);
-        }
-        this.queuedOutput = [];
-
-        for (const test of this.testItems) {
+        for (const test of addedTestItems) {
             this.enqueued(test);
         }
-    };
 
-    public addParameterizedTestCase = (testClass: TestClass, parentIndex: number) => {
-        this.addedTestItems.push({ testClass, parentIndex });
+        // Recreate a test item finder with the added test items
+        this.testItemFinder =
+            process.platform === "darwin"
+                ? new DarwinTestItemFinder(this.testItems)
+                : new NonDarwinTestItemFinder(this.testItems, this.folderContext);
     };
 
     public addAttachment = (testIndex: number, attachment: string) => {
@@ -486,8 +477,7 @@ export class TestRunner {
                   )
                 : new XCTestOutputParser();
         this.swiftTestOutputParser = new SwiftTestingOutputParser(
-            this.testRun.testRunStarted,
-            this.testRun.addParameterizedTestCase,
+            this.testRun.addParameterizedTestCases,
             this.testRun.addAttachment
         );
         this.onDebugSessionTerminated = this.debugSessionTerminatedEmitter.event;
@@ -501,8 +491,7 @@ export class TestRunner {
     public setIteration(iteration: number) {
         // The SwiftTestingOutputParser holds state and needs to be reset between iterations.
         this.swiftTestOutputParser = new SwiftTestingOutputParser(
-            this.testRun.testRunStarted,
-            this.testRun.addParameterizedTestCase,
+            this.testRun.addParameterizedTestCases,
             this.testRun.addAttachment
         );
         this.testRun.setIteration(iteration);
@@ -830,6 +819,8 @@ export class TestRunner {
                 // The await simply waits for the watching to be configured.
                 await this.swiftTestOutputParser.watch(fifoPipePath, runState);
 
+                this.testRun.testRunStarted();
+
                 await this.launchTests(
                     runState,
                     this.testKind === TestKind.parallel ? TestKind.standard : this.testKind,
@@ -858,7 +849,6 @@ export class TestRunner {
                 return this.testRun.runState;
             }
 
-            // XCTestRuns are started immediately
             this.testRun.testRunStarted();
 
             await this.launchTests(
