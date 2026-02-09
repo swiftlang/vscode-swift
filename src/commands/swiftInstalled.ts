09c7e57abf0441ac972222a3e9807c79dfc8890e
Gabe Braden <g_braden@apple.com>
2026-02-09 08:53:05 -0500

    Added liscense to pass CI

--- src/commands/swiftInstalled.ts
+++ src/commands/swiftInstalled.ts
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the VS Code Swift open source project
+//
+// Copyright (c) 2021-2026 the VS Code Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VS Code Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===/
 import * as child_process from "child_process";
 import { promisify } from "util";
 
