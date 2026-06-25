cc5172d02c5c490f9e2ad0f6b3b8782ff41fa419
Matthew Bastien <matthew_bastien@apple.com>
2026-06-25 13:53:12 -0400

    Poll for no running tasks instead of using onDidEndTask()
    Sometimes we miss the timing for the onDidEndTask() event which causes the tests to hang. Switch
    to polling instead to try and remedy this.

--- test/integration-tests/utilities/lsputilities.ts
+++ test/integration-tests/utilities/lsputilities.ts
@@ -20,27 +20,8 @@ import {
     WorkspaceSynchronizeRequest,
 } from "@src/sourcekit-lsp/extensions/PollIndexRequest";
 import { Disposable } from "@src/utilities/Disposable";
+import { poll } from "@src/utilities/utilities";
 import { Version } from "@src/utilities/version";
-
-type Result<T> = SuccessResult<T> | FailureResult;
-
-interface SuccessResult<T> {
-    type: "success";
-    value: T;
-}
-
-interface FailureResult {
-    type: "failure";
-}
-
-async function poll<T>(predicate: () => Promise<Result<T>>, interval: number = 100): Promise<T> {
-    let result = await predicate();
-    while (result.type === "failure") {
-        await new Promise<void>(resolve => setTimeout(resolve, interval));
-        result = await predicate();
-    }
-    return result.value;
-}
 
 /**
  * Wait for the LSP to indicate it is done indexing
@@ -85,25 +66,29 @@ export async function waitForCodeActions(
     uri: vscode.Uri,
     range: vscode.Range
 ): Promise<(langclient.CodeAction | langclient.Command)[]> {
-    return await poll<(langclient.CodeAction | langclient.Command)[]>(async () => {
-        try {
-            const response = await client.useLanguageClient((c, token) =>
-                c.sendRequest(
-                    langclient.CodeActionRequest.type,
-                    {
-                        context: langclient.CodeActionContext.create([]),
-                        textDocument: langclient.TextDocumentIdentifier.create(uri.toString()),
-                        range,
-                    },
-                    token
-                )
-            );
-            if (!response || response.length === 0) {
-                return { type: "failure" };
-            }
-            return { type: "success", value: response };
-        } catch (e) {
-            return { type: "failure" };
-        }
-    });
+    return await client.useLanguageClient((client, cancellationToken) =>
+        poll<(langclient.CodeAction | langclient.Command)[]>(
+            async () => {
+                try {
+                    const response = await client.sendRequest(
+                        langclient.CodeActionRequest.type,
+                        {
+                            context: langclient.CodeActionContext.create([]),
+                            textDocument: langclient.TextDocumentIdentifier.create(uri.toString()),
+                            range,
+                        },
+                        cancellationToken
+                    );
+                    if (!response || response.length === 0) {
+                        return { type: "failure" };
+                    }
+                    return { type: "success", value: response };
+                } catch (e) {
+                    return { type: "failure" };
+                }
+            },
+            500,
+            cancellationToken
+        )
+    );
 }
