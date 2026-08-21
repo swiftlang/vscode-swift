256804c898d0a12ed75bd32a8658d1d967ec8b83
Matthew Bastien <matthew_bastien@apple.com>
2026-08-21 10:57:53 -0400

    Add call site to errors produced by withTimeout()

--- test/utilities/withTimeout.ts
+++ test/utilities/withTimeout.ts
@@ -22,13 +22,14 @@ export type TimeoutTask<T> = (token: vscode.CancellationToken) => Promise<T>;
  * @param timeoutMs The timeout in milliseconds.
  */
 export function withTimeout<T>(task: TimeoutTask<T>, timeoutMs: number): Promise<T> {
+    const callSite = Error("withTimeout() was called here:");
     const cancellation = new vscode.CancellationTokenSource();
     let timeout: NodeJS.Timeout | undefined;
     return Promise.race([
         task(cancellation.token),
         new Promise<never>((_resolve, reject) => {
             timeout = setTimeout(() => {
-                reject(new Error(`Operation timed out after ${timeoutMs}ms`));
+                reject(new Error(`Operation timed out after ${timeoutMs}ms`, { cause: callSite }));
                 setImmediate(() => cancellation.cancel());
             }, timeoutMs);
         }),
